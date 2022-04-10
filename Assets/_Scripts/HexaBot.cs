c07bb846a9931c54484076129f977029fc3393c7
dongho shin <dongho5309@belivvr.com>
2022-04-10 16:02:50 +0900

    File Arrange

--- Assets/_Scripts/HexaBot.cs
+++ Assets/_Scripts/HexaBot.cs
@@ -1,20 +1,35 @@
+using HexaGround;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using HexaGround;
 
 public class HexaBot : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
+    public EHexaDirection direction = EHexaDirection.NONE;
+
     void Start()
     {
-        
+        for(int number = 0; number < (int)EHexaDirection.DIRCOUNT; ++number)
+        {
+            Move((EHexaDirection)number);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Move(EHexaDirection direction)
     {
-        
+        switch(direction)
+        {
+            case EHexaDirection.TOP_LEFT:
+            case EHexaDirection.TOP_RIGHT:
+            case EHexaDirection.DOWN_LEFT:
+            case EHexaDirection.DOWN_RIGHT:
+            case EHexaDirection.LEFT:
+            case EHexaDirection.RIGHT:
+                Debug.Log($"Move {direction}!");
+                break;
+            default:
+                Debug.Log("Wrong Direction");
+                break;
+        }
     }
 }
