58bd803c3b0bfb5320048017d7acc49331ebb87c
dongho shin <dongho5309@belivvr.com>
2022-04-10 15:27:39 +0900

    Editor folder, editor script of Altgrid

---
+++ Assets/Editor/AltGridEditor.cs
@@ -0,0 +1,13 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(AltGrid))]
+[CanEditMultipleObjects]
+public class AltGridEditor: Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+    }
+}
